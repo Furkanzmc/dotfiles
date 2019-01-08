ff66e5939b38794440c630c40bf96ee364bd90b4
Furkan Uzumcu <furkanuzumcu@gmail.com>
2019-01-08 13:55:38 -0500

    Add profile for Powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+﻿# Create an alias for `git stage`
+git config --global alias.unstage 'add'
+# Create an alias for `git unstage`
+git config --global alias.unstage 'reset HEAD --'
+# Create an alias to see the last commit `git last`
+git config --global alias.last 'log -1 HEAD'
+# Open GitX with `git visual`
+git config --global alias.visual '!open ./ -a "GitX"'
+# Use `git logall` to log all the changes
+git config --global alias.logall "log --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+# Use `git logcurrent` to log the current branch
+git config --global alias.logcurrent "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+# Use `git tasks` to list the TODO and FIXME entries in the code
+git config --global alias.tasks "ag 'TODO|FIXME' ./"
--- README.md
+++ README.md
@@ -8,3 +8,9 @@ Dotfiles for macOS
 - [bat](https://github.com/sharkdp/bat)
 - [exa](https://the.exa.website/)
 - [fd](https://github.com/sharkdp/fd)
+
+# Powershell Profile Path
+
+See [here](https://blogs.technet.microsoft.com/heyscriptingguy/2012/05/21/understanding-the-six-powershell-profiles/) for profile paths.
+
+Optionally check for `$profile` in Powershell and copy the file to that location.
