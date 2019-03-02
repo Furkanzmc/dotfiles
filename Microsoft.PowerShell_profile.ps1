6dd476df03127383c76591ac00c872e70c5a0110
Furkan Uzumcu <furkanuzumcu@gmail.com>
2019-03-01 20:03:02 -0800

    Update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
-﻿# Create an alias for `git stage`
+﻿# Git
+# Create an alias for `git stage`
 git config --global alias.unstage 'add'
 # Create an alias for `git unstage`
 git config --global alias.unstage 'reset HEAD --'
@@ -10,8 +11,14 @@ git config --global alias.visual '!open ./ -a "GitX"'
 git config --global alias.logall "log --graph --all --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 # Use `git logcurrent` to log the current branch
 git config --global alias.logcurrent "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.logpretty "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 # Use `git tasks` to list the TODO and FIXME entries in the code
 git config --global alias.tasks "ag 'TODO|FIXME' ./"
+git config --global color.status.changed "magenta normal bold"
+git config --global color.status.added "blue normal bold"
+git config --global color.status.unmerged "yellow normal bold"
+
+# -----
 
 # Settings for PSReadLine: https://github.com/lzybkr/PSReadLine
 Import-Module PSReadLine
