382ba24cfacb67fda847fac7de73382b6f9cc789
Furkan Uzumcu <furkanuzumcu@gmail.com>
2019-05-01 20:40:07 -0400

    Clean up
    - Remove the functions I don't use
    - Add todo

--- hammerspoon/windows-bindings.lua
+++ hammerspoon/windows-bindings.lua
@@ -12,49 +12,30 @@
 -- on WindowLayout mode. Then, use any shortcut below to perform a window layout
 -- action. For example, to send the window left, press and release
 -- Control+s, and then press h.
---
---   h/j/k/l => send window to the left/bottom/top/right half of the screen
---   i => send window to the upper left quarter of the screen
---   o => send window to the upper right quarter of the screen
---   , => send window to the lower left quarter of the screen
---   . => send window to the lower right quarter of the screen
---   return => make window full screen
---   n => send window to the next monitor
---   left => send window to the monitor on the left (if there is one)
---   right => send window to the monitor on the right (if there is one)
 --------------------------------------------------------------------------------
 
 return {
-  modifiers = {'alt'},
-  showHelp  = false,
-  trigger   = 'w',
-  mappings  = {
-    { {},         'd',      'close' },
-    { {},         'return', 'maximize' },
-    { {'shift'},  'c',      'centerWithFullHeight' },
-    { {},         'c',      'center' },
-    { {},         'h',      'left' },
-    { {},         'j',      'down' },
-    { {},         'k',      'up' },
-    { {},         'l',      'right' },
-    { {'shift'},  'h',      'leftSameSize' },
-    { {'shift'},  'l',      'rightSameSize' },
-    { {},         'i',      'upLeft' },
-    { {},         'o',      'upRight' },
-    { {},         ',',      'downLeft' },
-    { {},         '.',      'downRight' },
-    { {},         'n',      'nextScreen' },
-    { {},         'right',  'increaseWidth' },
-    { {},         'left',   'decreaseWidth' },
-    { {},         'up',  'increaseHeight' },
-    { {},         'down',   'decreaseHeight' },
-    { {},         '1',   'setDefaultTerminalSize' },
-    { {'shift'},         '\\',   'stretchHeight' },
-    { {'shift'},         '-',   'stretchWidth' },
-    { {},         '[',   'tileLeftHalfScreen' },
-    { {},         ']',   'tileRightHalfScreen' },
-    { {},         '=',   'tileFullScreen' },
-    { {'shift'},         '=',   'tileGrid' }
-  }
+    modifiers = {'alt'},
+    showHelp  = false,
+    trigger   = 'w',
+    mappings  = {
+        { {},         'd',      'close' },
+        { {},         'return', 'maximize' },
+        { {'shift'},  'c',      'centerWithFullHeight' },
+        { {},         'c',      'center' },
+        { {},         'h',      'left' },
+        { {},         'j',      'down' },
+        { {},         'k',      'up' },
+        { {},         'l',      'right' },
+        { {'shift'},  'h',      'leftSameSize' },
+        { {'shift'},  'l',      'rightSameSize' },
+        { {},         '1',      'setDefaultTerminalSize' },
+        { {'shift'},  '\\',     'stretchHeight' },
+        { {'shift'},  '-',      'stretchWidth' },
+        { {},         '[',      'tileLeftHalfScreen' },
+        { {},         ']',      'tileRightHalfScreen' },
+        { {},         '=',      'tileFullScreen' },
+        { {'shift'},  '=',      'tileGrid' }
+    }
 }
 
--- hammerspoon/windows.lua
+++ hammerspoon/windows.lua
@@ -1,3 +1,6 @@
+-- TODO:
+--     [ ] Restore window size.
+
 hs.window.animationDuration = 0.2
 
 function getWindows(win)
@@ -77,30 +80,6 @@ function hs.window.tileRightHalfScreen(win)
     end
 end
 
-function hs.window.increaseWidth(win)
-  local f = win:frame()
-  f.w = f.w + 20
-  win:setFrame(f)
-end
-
-function hs.window.decreaseWidth(win)
-  local f = win:frame()
-  f.w = f.w - 20
-  win:setFrame(f)
-end
-
-function hs.window.increaseHeight(win)
-  local f = win:frame()
-  f.h = f.h + 20
-  win:setFrame(f)
-end
-
-function hs.window.decreaseHeight(win)
-  local f = win:frame()
-  f.h = f.h - 20
-  win:setFrame(f)
-end
-
 function hs.window.setDefaultTerminalSize(win)
     if (win:application():name() ~= 'iTerm2') then
         return
@@ -116,23 +95,23 @@ function hs.window.setDefaultTerminalSize(win)
 end
 
 function hs.window.stretchHeight(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.y = 0
-  f.h = max.h
-  win:setFrame(f)
+    f.y = 0
+    f.h = max.h
+    win:setFrame(f)
 end
 
 function hs.window.stretchWidth(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.x = 0
-  f.w = max.w
-  win:setFrame(f)
+    f.x = 0
+    f.w = max.w
+    win:setFrame(f)
 end
 
 -- +-----------------+
@@ -141,25 +120,25 @@ end
 -- |        |        |
 -- +-----------------+
 function hs.window.left(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
 end
 
 function hs.window.leftSameSize(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  win:setFrame(f)
+    f.x = max.x
+    f.y = max.y
+    win:setFrame(f)
 end
 
 -- +-----------------+
@@ -168,25 +147,25 @@ end
 -- |        |        |
 -- +-----------------+
 function hs.window.right(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+    f.x = max.x + (max.w / 2)
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
 end
 
 function hs.window.rightSameSize(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.x = (max.x + max.w) - f.w
-  f.y = max.y
-  win:setFrame(f)
+    f.x = (max.x + max.w) - f.w
+    f.y = max.y
+    win:setFrame(f)
 end
 
 -- +-----------------+
@@ -195,15 +174,15 @@ end
 -- |                 |
 -- +-----------------+
 function hs.window.up(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.x = max.x
-  f.w = max.w
-  f.y = max.y
-  f.h = max.h / 2
-  win:setFrame(f)
+    f.x = max.x
+    f.w = max.w
+    f.y = max.y
+    f.h = max.h / 2
+    win:setFrame(f)
 end
 
 -- +-----------------+
@@ -212,84 +191,15 @@ end
 -- |      HERE       |
 -- +-----------------+
 function hs.window.down(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
 
-  f.x = max.x
-  f.w = max.w
-  f.y = max.y + (max.h / 2)
-  f.h = max.h / 2
-  win:setFrame(f)
-end
-
--- +-----------------+
--- |  HERE  |        |
--- +--------+        |
--- |                 |
--- +-----------------+
-function hs.window.upLeft(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:fullFrame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w/2
-  f.h = max.h/2
-  win:setFrame(f)
-end
-
--- +-----------------+
--- |                 |
--- +--------+        |
--- |  HERE  |        |
--- +-----------------+
-function hs.window.downLeft(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:fullFrame()
-
-  f.x = max.x
-  f.y = max.y + (max.h / 2)
-  f.w = max.w/2
-  f.h = max.h/2
-  win:setFrame(f)
-end
-
--- +-----------------+
--- |                 |
--- |        +--------|
--- |        |  HERE  |
--- +-----------------+
-function hs.window.downRight(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:fullFrame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y + (max.h / 2)
-  f.w = max.w/2
-  f.h = max.h/2
-
-  win:setFrame(f)
-end
-
--- +-----------------+
--- |        |  HERE  |
--- |        +--------|
--- |                 |
--- +-----------------+
-function hs.window.upRight(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:fullFrame()
-
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w/2
-  f.h = max.h/2
-  win:setFrame(f)
+    f.x = max.x
+    f.w = max.w
+    f.y = max.y + (max.h / 2)
+    f.h = max.h / 2
+    win:setFrame(f)
 end
 
 -- +--------------+
@@ -298,55 +208,42 @@ end
 -- |  |        |  |
 -- +---------------+
 function hs.window.centerWithFullHeight(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:fullFrame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:fullFrame()
 
-  f.x = max.x + (max.w / 5)
-  f.w = max.w * 3/5
-  f.y = max.y
-  f.h = max.h
-  win:setFrame(f)
+    f.x = max.x + (max.w / 5)
+    f.w = max.w * 3/5
+    f.y = max.y
+    f.h = max.h
+    win:setFrame(f)
 end
 
 function hs.window.center(win)
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:fullFrame()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:fullFrame()
 
-  f.x = max.x + (max.w / 2) - f.w / 2
-  f.y = max.y + (max.h / 2) - f.h / 2
-  win:setFrame(f)
-end
-
-function hs.window.nextScreen(win)
-  local currentScreen = win:screen()
-  local allScreens = hs.screen.allScreens()
-  currentScreenIndex = hs.fnutils.indexOf(allScreens, currentScreen)
-  nextScreenIndex = currentScreenIndex + 1
-
-  if allScreens[nextScreenIndex] then
-    win:moveToScreen(allScreens[nextScreenIndex])
-  else
-    win:moveToScreen(allScreens[1])
-  end
+    f.x = max.x + (max.w / 2) - f.w / 2
+    f.y = max.y + (max.h / 2) - f.h / 2
+    win:setFrame(f)
 end
 
 windowLayoutMode = hs.hotkey.modal.new({}, 'F16')
 
 windowLayoutMode.entered = function()
-  windowLayoutMode.statusMessage:show()
+    windowLayoutMode.statusMessage:show()
 end
 windowLayoutMode.exited = function()
-  windowLayoutMode.statusMessage:hide()
+    windowLayoutMode.statusMessage:hide()
 end
 
 -- Bind the given key to call the given function and exit WindowLayout mode
 function windowLayoutMode.bindWithAutomaticExit(mode, modifiers, key, fn)
-  mode:bind(modifiers, key, function()
-    mode:exit()
-    fn()
-  end)
+    mode:bind(modifiers, key, function()
+        mode:exit()
+        fn()
+    end)
 end
 
 local status, windowMappings = pcall(require, 'windows-bindings')
@@ -357,36 +254,36 @@ local trigger   = windowMappings.trigger
 local mappings  = windowMappings.mappings
 
 function getModifiersStr(modifiers)
-  local modMap = { shift = '⇧', ctrl = '⌃', alt = '⌥', cmd = '⌘' }
-  local retVal = ''
+    local modMap = { shift = '⇧', ctrl = '⌃', alt = '⌥', cmd = '⌘' }
+    local retVal = ''
 
-  for i, v in ipairs(modifiers) do
-    retVal = retVal .. modMap[v]
-  end
+    for i, v in ipairs(modifiers) do
+        retVal = retVal .. modMap[v]
+    end
 
-  return retVal
+    return retVal
 end
 
 local msgStr = getModifiersStr(modifiers)
 msgStr = 'Window Layout Mode (' .. msgStr .. (string.len(msgStr) > 0 and '+' or '') .. trigger .. ')'
 
 for i, mapping in ipairs(mappings) do
-  local modifiers, trigger, winFunction = table.unpack(mapping)
-  local hotKeyStr = getModifiersStr(modifiers)
+    local modifiers, trigger, winFunction = table.unpack(mapping)
+    local hotKeyStr = getModifiersStr(modifiers)
 
-  if showHelp == true then
-    if string.len(hotKeyStr) > 0 then
-      msgStr = msgStr .. (string.format('\n%10s+%s => %s', hotKeyStr, trigger, winFunction))
-    else
-      msgStr = msgStr .. (string.format('\n%11s => %s', trigger, winFunction))
+    if showHelp == true then
+        if string.len(hotKeyStr) > 0 then
+            msgStr = msgStr .. (string.format('\n%10s+%s => %s', hotKeyStr, trigger, winFunction))
+        else
+            msgStr = msgStr .. (string.format('\n%11s => %s', trigger, winFunction))
+        end
     end
-  end
 
-  windowLayoutMode:bindWithAutomaticExit(modifiers, trigger, function()
-    --example: hs.window.focusedWindow():upRight()
-    local fw = hs.window.focusedWindow()
-    fw[winFunction](fw)
-  end)
+    windowLayoutMode:bindWithAutomaticExit(modifiers, trigger, function()
+        --example: hs.window.focusedWindow():upRight()
+        local fw = hs.window.focusedWindow()
+        fw[winFunction](fw)
+    end)
 end
 
 local message = require('status-message')
@@ -394,9 +291,9 @@ windowLayoutMode.statusMessage = message.new(msgStr)
 
 -- Use modifiers+trigger to toggle WindowLayout Mode
 hs.hotkey.bind(modifiers, trigger, function()
-  windowLayoutMode:enter()
+    windowLayoutMode:enter()
 end)
 windowLayoutMode:bind(modifiers, trigger, function()
-  windowLayoutMode:exit()
+    windowLayoutMode:exit()
 end)
 
