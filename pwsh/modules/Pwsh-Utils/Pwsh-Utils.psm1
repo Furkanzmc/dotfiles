04373fff29fc6166459226fcb8be3add9d2fb186
Furkan Uzumcu <furkanuzumcu@gmail.com>
2020-04-15 16:57:19 -0400

    Add some utilities with fzf

--- pwsh/modules/Pwsh-Utils/Pwsh-Utils.psm1
+++ pwsh/modules/Pwsh-Utils/Pwsh-Utils.psm1
@@ -11,6 +11,64 @@ function Virtualenv-Activate() {
     }
 }
 
+if (Get-Command "fzf" -ErrorAction SilentlyContinue) {
+    function Fzf-History() {
+        Param(
+            [Parameter(Position=0, Mandatory=$false)]
+            [Switch]
+            $All=$false
+        )
+
+        $count = 1000
+        if ($All) {
+            $count = -1
+        }
+
+        $selectedItem = Get-Content (Get-PSReadlineOption).HistorySavePath -Tail $count | fzf --reverse --tac
+        Write-Host $selectedItem -ForegroundColor blue
+        Set-Clipboard $selectedItem
+    }
+
+    function Add-Dir-Bookmark() {
+        $location = Get-Location
+        Add-Content -Path "$HOME/.dotfiles/pwsh/tmp_dirs/bookmarks.txt" `
+            -Value $location.Path
+    }
+
+    function Clear-Dir-Bookmarks() {
+        $location = Get-Location
+        Set-Content -Path "$HOME/.dotfiles/pwsh/tmp_dirs/bookmarks.txt" `
+            -Value ""
+    }
+
+    function Get-Dir-Bookmarks() {
+        $bookmarkPath = "$HOME/.dotfiles/pwsh/tmp_dirs/bookmarks.txt"
+        if (Test-Path $bookmarkPath -ErrorAction SilentlyContinue) {
+            Get-Content $bookmarkPath | Sort-Object -Unique `
+                | fzf --reverse +x | Set-Location
+        }
+        else {
+            Write-Host "No bookmarks file."
+        }
+    }
+
+    function Get-Commands() {
+        $selected = Get-Command -All | Select-Object -Property CommandType,Name `
+            | fzf --reverse +x
+        if ($selected) {
+            $selected = $selected.Trim().Split(" ")[1]
+            Write-Host $selected -ForegroundColor blue
+            Set-Clipboard $selected
+        }
+    }
+
+    Set-Alias -Name hh -Value Fzf-History
+    Set-Alias -Name mm -Value Add-Dir-Bookmark
+    Set-Alias -Name ms -Value Get-Dir-Bookmarks
+    Set-Alias -Name mc -Value Clear-Dir-Bookmarks
+    Set-Alias -Name commands -Value Get-Commands
+}
+
 function Copy-Pwd() {
     if ($IsMacOS) {
         (pwd).Path | pbcopy
