d2b53ac29353b60d1cb7f73189643615704a38ed
Furkan Uzumcu <furkanuzumcu@gmail.com>
2020-12-05 13:44:44 -0500

    Fix completion plugin and add for additional sources

--- vim/after/ftplugin/vim.vim
+++ vim/after/ftplugin/vim.vim
@@ -1,1 +1,3 @@
 setlocal foldmethod=marker
+
+let b:vimrc_completion_additional_sources = ["<c-x><c-v>"]
--- vim/lua/vimrc/completion.lua
+++ vim/lua/vimrc/completion.lua
@@ -4,15 +4,86 @@ local M = {}
 local s_last_cursor_position = nil
 local s_completion_timer = nil
 local s_completion_sources = {
-    "<c-x><c-o>",
-    "<c-x><c-n>",
-    "<c-x><c-f>",
-    "<c-x><c-k>",
-    "<c-g><c-g><c-n>"
+    omni={
+        keys="<c-x><c-o>",
+        prediciate=function()
+            return vim.bo.omnifunc ~= "" or vim.o.omnifunc ~= ""
+        end
+    },
+    keywords_current={
+        keys="<c-x><c-n>",
+    },
+    file={
+        keys="<c-x><c-f>",
+    },
+    dictionary={
+        keys="<c-x><c-k>",
+        prediciate=function()
+            return pcall(vim.api.nvim_buf_get_option, '.', "dictionary") or pcall(vim.api.nvim_get_option, '.', "dictionary")
+        end
+    },
+    keywords={
+        keys="<c-g><c-g><c-n>",
+    },
+    spell={
+        keys="<c-x><c-s>",
+        prediciate=function()
+            return vim.wo.spell
+        end
+    },
+    user={
+        keys="<c-x><c-u>",
+        prediciate=function()
+            return vim.bo.completefunc ~= "" or vim.o.completefunc ~= ""
+        end
+    },
 }
 local s_completion_index = nil
 local s_is_completion_dispatched = false
 local s_plugin_loaded = false
+local s_buffer_completion_sources_cache = {}
+local s_buffer_completion_source_names_cache = {}
+
+function get_completion_sources(bufnr)
+    if s_buffer_completion_sources_cache[bufnr] ~= nil then
+        return s_buffer_completion_sources_cache[bufnr]
+    end
+
+    local result = pcall(vim.api.nvim_buf_get_var, bufnr, "vimrc_completion_additional_sources")
+    if result == false then
+        return s_completion_sources
+    end
+
+    local additional_sources = vim.api.nvim_buf_get_var(bufnr, "vimrc_completion_additional_sources")
+    local new_list = {}
+
+    for key, value in pairs(s_completion_sources) do
+        new_list[key] = value
+    end
+
+    for index, value in ipairs(additional_sources) do
+        new_list["custom_" .. index] = {keys=value}
+    end
+
+    s_buffer_completion_sources_cache[bufnr] = new_list
+    return new_list
+end
+
+function get_source_names(bufnr)
+    if s_buffer_completion_source_names_cache[bufnr] ~= nil then
+        return s_buffer_completion_source_names_cache[bufnr]
+    end
+
+    local sources = get_completion_sources(bufnr)
+    local names = {}
+
+    for key,_ in pairs(sources) do
+        table.insert(names, key)
+    end
+
+    s_buffer_completion_source_names_cache[bufnr] = names
+    return names
+end
 
 function timer_handler()
     if s_completion_index == -1 then
@@ -24,18 +95,22 @@ function timer_handler()
         return
     end
 
+    local bufnr = vim.fn.bufnr()
+    local completion_sources = get_completion_sources(bufnr)
+    local completion_source_names = get_source_names(bufnr)
+
     if vim.fn.pumvisible() == 0 then
-        if s_completion_index == #s_completion_sources + 1 then
+        if s_completion_index == #completion_source_names + 1 then
             s_is_completion_dispatched = false
         else
-            local mode_keys = s_completion_sources[s_completion_index]
-
-            if mode_keys == "<c-x><c-k>" and not pcall(vim.api.nvim_buf_get_option, '.', "dictionary") then
+            local source = completion_sources[completion_source_names[s_completion_index]]
+            if source.prediciate ~= nil and source.prediciate() == false then
                 s_completion_index = s_completion_index + 1
-                mode_keys = s_completion_sources[s_completion_index]
+                timer_handler()
+                return
             end
 
-            mode_keys = vim.api.nvim_replace_termcodes(mode_keys, true, false, true)
+            local mode_keys = vim.api.nvim_replace_termcodes(source.keys, true, false, true)
             vim.api.nvim_feedkeys(mode_keys, 'n', true)
             s_is_completion_dispatched = true
             s_completion_index = s_completion_index + 1
@@ -76,13 +151,14 @@ M.on_complete_done_pre = function()
     s_completion_timer:start(100, 0, vim.schedule_wrap(timer_handler))
 end
 
-M.on_complete_done = function()
+M.on_complete_done = function(bufnr)
     if s_is_completion_dispatched == true then
         return
     end
 
+    local completion_sources = get_completion_sources(bufnr)
     local cursorPosition = vim.api.nvim_win_get_cursor(0)
-    if cursorPosition[1] == s_last_cursor_position[1] and cursorPosition[2] == s_last_cursor_position[2] and s_completion_index == #s_completion_sources + 1 then
+    if cursorPosition[1] == s_last_cursor_position[1] and cursorPosition[2] == s_last_cursor_position[2] and s_completion_index == #completion_sources + 1 then
         vim.api.nvim_feedkeys(
             vim.api.nvim_replace_termcodes("<c-y>", true, false, true), "n",
             true
@@ -114,19 +190,23 @@ end
 
 
 M.setup_completion = function()
-    if s_plugin_loaded == true then
+    local bufnr = vim.api.nvim_get_current_buf()
+
+    if vim.fn.exists("b:vimrc_is_completion_configured") == 0 then
+        vim.api.nvim_buf_set_var(bufnr, "vimrc_is_completion_configured", false)
+    elseif vim.api.nvim_buf_get_var(bufnr, "vimrc_is_completion_configured") == 1 then
         return
     end
 
-    s_plugin_loaded = true
-
-    vim.api.nvim_command("augroup vimrc_completion")
+    vim.api.nvim_command("augroup vimrc_completion_buf_" .. bufnr)
     vim.api.nvim_command("au!")
     vim.api.nvim_command(
-        "autocmd CompleteDonePre lua require'vimrc.completion'.on_complete_done_pre()")
+        "autocmd CompleteDonePre <buffer=" .. bufnr .. "> lua require'vimrc.completion'.on_complete_done_pre()")
     vim.api.nvim_command(
-        "autocmd CompleteDone lua require'vimrc.completion'.on_complete_done()")
+        "autocmd CompleteDone <buffer=" .. bufnr .. "> lua require'vimrc.completion'.on_complete_done(" .. bufnr .. ")")
     vim.api.nvim_command("augroup END")
+
+    vim.api.nvim_buf_set_var(bufnr, "vimrc_is_completion_configured", true)
 end
 
 return M
