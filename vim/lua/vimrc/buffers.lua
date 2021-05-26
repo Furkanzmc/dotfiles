4e638f22bc3893b0813d19c7bce0e9deeb63d621
Furkan Uzumcu <furkanuzumcu@gmail.com>
2021-05-25 22:26:03 -0400

    Update

--- vim/lua/vimrc/buffers.lua
+++ vim/lua/vimrc/buffers.lua
@@ -181,6 +181,10 @@ function M.init()
     options.register_callback("markdownfenced", function()
         local langs = options.get_option("markdownfenced",
                                          vim.api.nvim_get_current_buf())
+        if g.markdown_fenced_languages == nil then
+            g.markdown_fenced_languages = {}
+        end
+
         g.markdown_fenced_languages = table.uniq(
                                           table.extend(
                                               g.markdown_fenced_languages, langs))
