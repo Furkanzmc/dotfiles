65ab09103daf096fe4e75ba0b8ac41505f366a59
Furkan Uzumcu <furkanuzumcu@gmail.com>
2021-05-29 19:44:48 -0400

    Update

--- install.ps1
+++ install.ps1
@@ -1,15 +1,20 @@
-Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
-
 if ($IsWindows) {
     Install-Module Pscx -Scope CurrentUser -AllowClobber
     Install-Module VSSetup -Scope CurrentUser
 }
 
-echo '. "~/.dotfiles/pwsh_profile.ps1"' >> $profile
-New-Item -Force -ItemType SymbolicLink -Path "~/.dotfiles/tmux.conf" -Target "~/.tmux.conf"
-New-Item -Force -ItemType SymbolicLink -Path "~/.dotfiles/terminals/alacritty.yml" -Target "~/.alacritty.yml"
+echo '. "~/.dotfiles/pwsh/profile.ps1"' >> $profile
+if ($IsWindows) {
+    echo '$env:PSModulePath += $([System.IO.Path]::PathSeparator) + ~/.dotfiles/pwsh/modules/'
+    echo '. "~/.dotfiles/pwsh/pwsh_profile.ps1"' >> $profile
+}
+New-Item -Force -ItemType SymbolicLink -Path "~/.tmux.conf" -Target "~/.dotfiles/tmux.conf"
 
-New-Item -Force -ItemType SymbolicLink -Path "~/.dotfiles/karabiner_vi_style.json" -Target "~/.config/karabiner/assets/complex_modifications/karabiner_vi_style.json"
+New-Item -ItemType Directory -Force -Path "~/.config/alacritty"
+New-Item -Force -ItemType SymbolicLink -Path "~/.config/alacritty/alacritty.yml" -Target "~/.dotfiles/terminals/alacritty.yml"
+
+New-Item -ItemType Directory -Force -Path "~/.config/karabiner/assets/complex_modifications/"
+New-Item -Force -ItemType SymbolicLink -Path "~/.config/karabiner/assets/complex_modifications/karabiner_vi_style.json" -Target "~/.dotfiles/karabiner_vi_style.json"
 
 New-Item -ItemType Directory -Force -Path "~/.hammerspoon"
 
@@ -17,5 +22,3 @@ foreach ($file in Get-ChildItem -Path "~/.dotfiles/hammerspoon/") {
     $fileName = Split-Path $file -Leaf
     New-Item -Force -ItemType SymbolicLink -Target $file -Path ~/.hammerspoon/$fileName
 }
-
-New-Item -Force -ItemType SymbolicLink -Path "~/.dotfiles/terminals/iterm2.json" -Target "~/Library/Application\ Support/iTerm2/DynamicProfiles"
