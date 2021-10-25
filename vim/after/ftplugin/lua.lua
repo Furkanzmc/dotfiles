4f7f304dbea353e0e3c583804b3494d3176ebc74
Furkan Uzumcu <furkanuzumcu@gmail.com>
2021-10-24 20:44:50 -0600

    Update

--- vim/after/ftplugin/lua.lua
+++ vim/after/ftplugin/lua.lua
@@ -4,6 +4,7 @@ vim.bo.suffixesadd = ".lua"
 vim.wo.foldmethod = "expr"
 vim.wo.colorcolumn = "81,101"
 vim.bo.textwidth = 100
+vim.wo.signcolumn = "number"
 
 vim.b.vimrc_sumneko_lua_lsp_signs_enabled = 1
 vim.b.vimrc_sumneko_lua_lsp_location_list_enabled = 1
