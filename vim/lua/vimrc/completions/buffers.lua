38447873bf8758157e14a59108637bae7f67c66f
Furkan Uzumcu <furkanuzumcu@gmail.com>
2021-11-02 23:06:54 -0400

    Refactor buffers source

--- vim/lua/vimrc/completions/buffers.lua
+++ vim/lua/vimrc/completions/buffers.lua
@@ -1,17 +1,11 @@
--- Buffer completion taken from https://github.com/hrsh7th/cmp-buffer
----@class cmp_buffer.Buffer
----@field public bufnr number
----@field public regexes any[]
----@field public pattern string
----@field public timer any|nil
----@field public words table<number, string[]>
----@field public processing boolean
+local s_defaults = {
+    keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%([\-]\w*\)*\)]],
+    get_bufnrs = function()
+        return table.uniq(vim.fn.tabpagebuflist(vim.fn.tabpagenr()))
+    end,
+}
 local buffer = {}
 
----Create new buffer object
----@param bufnr number
----@param pattern string
----@return cmp_buffer.Buffer
 function buffer.new(bufnr, pattern)
     local self = setmetatable({}, { __index = buffer })
     self.bufnr = bufnr
@@ -23,7 +17,6 @@ function buffer.new(bufnr, pattern)
     return self
 end
 
----Close buffer
 function buffer.close(self)
     if self.timer then
         self.timer:stop()
@@ -33,7 +26,6 @@ function buffer.close(self)
     self.words = {}
 end
 
----Indexing buffer
 function buffer.index(self)
     self.processing = true
     local index = 1
@@ -63,7 +55,6 @@ function buffer.index(self)
     )
 end
 
---- watch
 function buffer.watch(self)
     vim.api.nvim_buf_attach(self.bufnr, false, {
         on_lines = vim.schedule_wrap(
@@ -73,17 +64,14 @@ function buffer.watch(self)
                     return true
                 end
 
-                -- append
                 for i = old_lastline, new_lastline - 1 do
                     table.insert(self.words, i + 1, {})
                 end
 
-                -- remove
                 for _ = new_lastline, old_lastline - 1 do
                     table.remove(self.words, new_lastline + 1)
                 end
 
-                -- replace lines
                 local lines = vim.api.nvim_buf_get_lines(self.bufnr, firstline, new_lastline, false)
                 vim.api.nvim_buf_call(self.bufnr, function()
                     for i, line in ipairs(lines) do
@@ -97,11 +85,10 @@ function buffer.watch(self)
     })
 end
 
---- add_words
 function buffer.index_line(self, i, line)
     local words = {}
-
     local buf = line
+
     while true do
         local s, e = self:matchstrpos(buf)
         if s then
@@ -120,7 +107,6 @@ function buffer.index_line(self, i, line)
     self.words[i] = words
 end
 
---- get_words
 function buffer.get_words(self)
     local words = {}
     for _, line in ipairs(self.words) do
@@ -128,30 +114,23 @@ function buffer.get_words(self)
             table.insert(words, w)
         end
     end
+
     return words
 end
 
---- matchstrpos
 function buffer.matchstrpos(self, text)
     local s, e = self:regex(self.pattern):match_str(text)
     if s == nil then
         return nil, nil
     end
+
     return s + 1, e + 1
 end
 
---- regex
 function buffer.regex(self, pattern)
     self.regexes[pattern] = self.regexes[pattern] or vim.regex(pattern)
     return self.regexes[pattern]
 end
-
-local defaults = {
-    keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%([\-]\w*\)*\)]],
-    get_bufnrs = function()
-        return table.uniq(vim.fn.tabpagebuflist(vim.fn.tabpagenr()))
-    end,
-}
 
 local source = {}
 
@@ -161,41 +140,9 @@ source.new = function()
     return self
 end
 
-source.get_keyword_pattern = function(_, params)
-    params.option = vim.tbl_deep_extend("keep", params.option, defaults)
-    vim.validate({
-        keyword_pattern = {
-            params.option.keyword_pattern,
-            "string",
-            "`opts.keyword_pattern` must be `string`",
-        },
-        get_bufnrs = {
-            params.option.get_bufnrs,
-            "function",
-            "`opts.get_bufnrs` must be `function`",
-        },
-    })
-    return params.option.keyword_pattern
-end
-
 source.complete = function(self, params, callback)
-    params.option = params.option or {}
-    params.option = vim.tbl_deep_extend("keep", params.option, defaults)
-    vim.validate({
-        keyword_pattern = {
-            params.option.keyword_pattern,
-            "string",
-            "`opts.keyword_pattern` must be `string`",
-        },
-        get_bufnrs = {
-            params.option.get_bufnrs,
-            "function",
-            "`opts.get_bufnrs` must be `function`",
-        },
-    })
-
     local processing = false
-    for _, buf in ipairs(self:_get_buffers(params)) do
+    for _, buf in ipairs(self:_get_buffers(s_defaults.get_bufnrs(), s_defaults.keyword_pattern)) do
         processing = processing or buf.processing
     end
 
@@ -204,13 +151,13 @@ source.complete = function(self, params, callback)
             local input = params.word_to_complete
             local items = {}
             local words = {}
-            for _, buf in ipairs(self:_get_buffers(params)) do
+            for _, buf in ipairs(self:_get_buffers(s_defaults.get_bufnrs(), s_defaults.keyword_pattern)) do
                 for _, word in ipairs(buf:get_words()) do
                     if not words[word] and input ~= word then
                         words[word] = true
                         table.insert(items, {
                             label = word,
-                            kind = 1,
+                            kind = vim.lsp.protocol.CompletionItemKind["Text"],
                             dup = 0,
                         })
                     end
@@ -218,21 +165,16 @@ source.complete = function(self, params, callback)
             end
 
             callback(items)
-            -- callback({
-            --     items = items,
-            --     isIncomplete = processing,
-            -- })
         end),
         processing and 100 or 0
     )
 end
 
---- _get_bufs
-source._get_buffers = function(self, params)
+source._get_buffers = function(self, bufnrs, keyword_pattern)
     local buffers = {}
-    for _, bufnr in ipairs(params.option.get_bufnrs()) do
+    for _, bufnr in ipairs(bufnrs) do
         if not self.buffers[bufnr] then
-            local new_buf = buffer.new(bufnr, params.option.keyword_pattern)
+            local new_buf = buffer.new(bufnr, keyword_pattern)
             new_buf:index()
             new_buf:watch()
             self.buffers[bufnr] = new_buf
