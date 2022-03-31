de9572501d5321b7217aed1ff82dbe39d179435d
Furkan Uzumcu <furkanuzumcu@gmail.com>
2022-03-30 23:16:14 -0400

    Update

--- vim/after/ftdetect/filetype.lua
+++ vim/after/ftdetect/filetype.lua
@@ -1,6 +1,5 @@
 vim.cmd([[augroup vimrc_filetypes]])
 vim.cmd([[au!]])
-vim.cmd([[autocmd BufNewFile,BufRead hfl.txt setlocal filetype=hfl]])
 vim.cmd([[autocmd BufNewFile,BufRead term://* setlocal filetype=terminal]])
 vim.cmd([[autocmd TermOpen * setlocal filetype=terminal]])
 vim.cmd([[autocmd BufNewFile,BufRead todo.txt setlocal filetype=todo]])
