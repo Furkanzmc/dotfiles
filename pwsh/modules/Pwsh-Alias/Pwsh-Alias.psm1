6a916a027d15c9f1cefcf9390100d2bc9f9cac0b
Furkan Uzumcu <furkanuzumcu@gmail.com>
2022-10-19 08:09:48 -0400

    Update

--- pwsh/modules/Pwsh-Alias/Pwsh-Alias.psm1
+++ pwsh/modules/Pwsh-Alias/Pwsh-Alias.psm1
@@ -7,22 +7,7 @@ function global:vit() {
 }
 
 function global:vnote() {
-    if (-not (Test-Path env:ZNOTES_GIT_DIR)) {
-        Write-Host -ForegroundColor red "ZNOTES_GIT_DIR environment variable is reuired."
-        return
-    }
-
-    if (-not (Test-Path env:ZNOTES_DIR)) {
-        Write-Host -ForegroundColor red "ZNOTES_DIR environment variable is reuired."
-        return
-    }
-
-    if (-not (Test-Path $env:ZNOTES_DIR/.nvimrc)) {
-        Write-Host -ForegroundColor red "$env:ZNOTES_DIR/.nvimrc file doesn't exist."
-        return
-    }
-
-    nvim -S $env:ZNOTES_DIR/.nvimrc "+ZkBrowse"
+    nvim "+ZkBrowse"
 }
 
 function Pwsh-Exit() {
