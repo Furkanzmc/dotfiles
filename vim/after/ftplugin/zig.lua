6658876338bb549a283b7a3aef5b18bd022c7c2e
Furkan Uzumcu <furkanuzumcu@gmail.com>
2022-11-01 22:00:44 -0400

    Update

---
+++ vim/after/ftplugin/zig.lua
@@ -0,0 +1,18 @@
+if vim.b.did_ftp == true then
+    return
+end
+
+if vim.g.vimrc_zig_loaded_plugins == nil and vim.o.loadplugins then
+    vim.cmd([[packadd zig.vim]])
+    vim.g.vimrc_zig_loaded_plugins = true
+end
+
+vim.wo.signcolumn = "yes"
+vim.opt_local.cursorline = false
+vim.opt_local.cursorcolumn = false
+
+if vim.fn.executable("zig") == 1 then
+    vim.opt_local.formatprg = "zig fmt --stdin --check"
+end
+
+vim.b.vimrc_null_ls_lsp_signs_enabled = 1
