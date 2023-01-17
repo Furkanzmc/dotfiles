7d7c47e477b0bd98df0d2580ce8b5814eead5838
Furkan Uzumcu <furkanuzumcu@gmail.com>
2023-01-17 10:44:08 -0500

    Update

--- vim/after/ftplugin/gitrebase.lua
+++ vim/after/ftplugin/gitrebase.lua
@@ -3,9 +3,8 @@ if vim.b.did_ftp == true then
 end
 
 vim.opt_local.colorcolumn = ""
-vim.opt_local.number = false
+vim.opt_local.number = true
+vim.opt_local.relativenumber = true
 vim.opt_local.cursorline = true
 vim.opt_local.cursorcolumn = false
 vim.opt_local.signcolumn = "no"
-
-vim.opt_local.relativenumber = false
