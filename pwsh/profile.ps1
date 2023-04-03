73f80a41715139cd7f475ed793a48f29eb5fddc1
Furkan Uzumcu <furkanuzumcu@gmail.com>
2023-04-03 08:24:26 -0400

    Update

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -4,9 +4,7 @@ if ((Test-Path env:VIRTUAL_ENV) -and (Test-Path "${env:VIRTUAL_ENV}/bin/activate
     . ${env:VIRTUAL_ENV}/bin/activate.ps1
 }
 
-if (-not [System.Environment]::UserInteractive) {
-    $PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;
-}
+$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;
 
 if ($IsWindows) {
     $env:PSModulePath += ";~/.dotfiles/pwsh/modules/"
