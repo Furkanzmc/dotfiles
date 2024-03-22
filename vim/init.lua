358ee356e668aa094e8f4de64e12e610615222a1
Furkan Uzumcu <furkanuzumcu@gmail.com>
2024-03-22 15:56:21 -0700

    Update

--- vim/init.lua
+++ vim/init.lua
@@ -261,6 +261,7 @@ g.neovide_cursor_vfx_mode = "sonicboom"
 g.neovide_cursor_trail_size = 0.5
 g.neovide_hide_mouse_when_typing = true
 g.neovide_scroll_animation_length = 0.0
+g.neovide_cursor_animation_length = 0.02
 
 -- }}}
 
