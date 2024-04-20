28bfbca1df3432a8111f78242a27259e36fafe15
Furkan Uzumcu <furkanuzumcu@gmail.com>
2024-04-20 19:38:11 -0400

    Update

---
+++ vim/after/ftplugin/dart.lua
@@ -0,0 +1,14 @@
+if vim.b.did_ftp == true then
+    return
+end
+
+vim.opt_local.cindent = false
+vim.opt_local.suffixesadd = ".dart"
+vim.opt_local.cursorcolumn = false
+vim.opt_local.signcolumn = "yes"
+vim.opt_local.textwidth = 100
+vim.opt_local.formatprg = "dart format"
+vim.opt_local.makeprg = "dart $*"
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
