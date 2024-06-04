b2d797e3783bf0d047e21174b2573ac3310b0a35
Furkan Uzumcu <furkanuzumcu@gmail.com>
2024-06-04 18:19:49 -0400

    Update

--- vim/after/ftplugin/cmake.lua
+++ vim/after/ftplugin/cmake.lua
@@ -8,5 +8,6 @@ local b = vim.b
 opt_local.textwidth = 100
 opt_local.signcolumn = "yes"
 opt_local.cursorcolumn = false
+opt_local.formatprg = "cmake-format -"
 
 b.vimrc_null_ls_lsp_signs_enabled = 1
