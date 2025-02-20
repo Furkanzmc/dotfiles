3b35c78779397c691fc13d9633f973f97dce6361
Furkan Uzumcu <furkanuzumcu@gmail.com>
2025-02-20 10:29:16 -0500

    Update

--- vim/after/ftplugin/tagbar.lua
+++ vim/after/ftplugin/tagbar.lua
@@ -4,7 +4,8 @@ end
 
 vim.wo.number = false
 vim.wo.relativenumber = false
-vim.opt_local.cursorline = false
+vim.opt_local.cursorline = true
+vim.opt_local.cursorlineopt = "number,line"
 vim.opt_local.cursorcolumn = false
 vim.opt_local.signcolumn = "no"
 vim.opt_local.winbar = ""
